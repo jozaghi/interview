0af076bce6f69244fe9879b12b63b90885d79492
Alinull <info@alinull.com>
2020-01-05 21:10:53 -0800

    most common word

---
+++ Interview.Test/LeetCode/MostCommonWordTest.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Interview.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Interview.Test.LeetCode
+{
+    public class MostCommonWordTest
+    {
+        [Theory]
+        [InlineData("Bob hit a ball, the hit BALL flew far after it was hit.", new string[] { "hit" },"ball")]
+        public void TestFind(string paragraph, string[] bannedList, string expected)
+        {
+            var m = new MostCommonWord();
+            var result = m.Find(paragraph, bannedList);
+
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ Interview/LeetCode/MostCommonWord.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interview.LeetCode
+{
+    public class MostCommonWord
+    {
+        public string Find(string paragraph, string[] banned)
+        {
+            var banSet = new HashSet<string>(banned);
+            var world = string.Empty;
+            var visited = new Dictionary<string, int>();
+            for (int i = 0; i < paragraph.Length; i++)
+            {
+                var currentChar = paragraph[i];
+
+                if (currentChar == ' ' || char.IsPunctuation(currentChar))
+                {
+                    SetNewWorld(world, banSet, visited);
+                    world = string.Empty;
+                }
+                else
+                {
+                    world += currentChar;
+                }
+            }
+            SetNewWorld(world, banSet, visited);
+
+            var mostVisitedWorld = string.Empty;
+            var mostVisitedNum = 0;
+            foreach (var item in visited)
+            {
+                if (item.Value > mostVisitedNum)
+                {
+                    mostVisitedWorld = item.Key;
+                    mostVisitedNum = item.Value;
+                }
+            }
+            return mostVisitedWorld;
+        }
+
+        private void SetNewWorld(string world, HashSet<string> banSet, Dictionary<string, int> visited)
+        {
+            if (world != string.Empty && !banSet.Contains(world.ToLower()))
+            {
+                var key = world.ToLower();
+                visited[key] =
+                    (visited.ContainsKey(key) ? visited[key] : 0) +
+                    1;
+
+            }
+        }
+    }
+}
